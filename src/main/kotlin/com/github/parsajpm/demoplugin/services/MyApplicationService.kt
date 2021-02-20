373162dc87f160edf6d66b6afa789aec11418eff
GitHub Action <action@github.com>
2021-02-20 11:03:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/parsajpm/demoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/parsajpm/demoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.parsajpm.demoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.parsajpm.demoplugin.MyBundle
 
 class MyApplicationService {
 
